5d35704e7f06a734a67a7dc6d0384a17b7bc6202
devmfk83 <hatirghil@gmail.com>
2026-09-13 11:42:27 +0600

    first commit

---
+++ src/Components/Nav.tsx
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import Logo from "../assets/logo-text.png";
+
+const Nav = () =>{
+    return (
+            <nav className='bg-amber-50 pt-5 pb-5'>
+                <div className=' max-auto flex justify-around'>
+                  <img src={Logo} alt="logo-text"/>
+                  
+                <ul className="flex gap-6 items-center ">
+                    <li>Home</li>
+                   <li>Technologies</li>
+                   <li>Projects</li>
+                   <li>About</li>
+                   <li>Contact</li>
+                 </ul>
+        
+                        <div>
+                           <input type="reset" value="Sign In" className="btn" />
+                           <button className="btn btn-secondary">Sign Up</button>
+                        </div>
+                </div>
+        </nav>
+    );
+};
+
+export default Nav;
